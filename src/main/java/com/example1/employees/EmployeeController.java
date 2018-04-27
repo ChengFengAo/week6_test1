62045d5f8574d875361bd6fee52ff455db44d4b4
ChengFeng AO <414356022@qq.com>
2018-04-27 15:45:04 +0800

    add function addEmployee

--- src/main/java/com/example1/employees/EmployeeController.java
+++ src/main/java/com/example1/employees/EmployeeController.java
@@ -1,8 +1,7 @@
 package com.example1.employees;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,9 +9,19 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
-    @PostMapping(value = "/employees")
+    @GetMapping(value = "/employees")
     public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
     }
+
+    @PostMapping(value = "/addEmployee")
+    public void addEmployee(@RequestParam String name,@RequestParam int age,@RequestParam String gender) {
+          Employee newEmployee= new Employee();
+          newEmployee.setName(name);
+          newEmployee.setAge(age);
+          newEmployee.setGender(gender);
+          employeeRepository.save(newEmployee);
+    }
+    
 
 }
